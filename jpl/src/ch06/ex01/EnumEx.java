733dd35c1c82079a75beaa7b42762e830701878e
Tomom1-S <Tomomi.Suzuki1@jp.ricoh.com>
2019-10-14 12:10:47 +0900

    6.1

---
+++ jpl/src/ch06/ex01/EnumEx.java
@@ -0,0 +1,10 @@
+package ch06.ex01;
+
+public class EnumEx {
+    enum DayOfWeek {
+        SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+    }
+    enum TrafficLight {
+        GREEN, YELLOW, RED
+    }
+}
