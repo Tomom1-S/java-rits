1f4eae656e9551da02cbd74774c31056cc7e5c04
Tomom1-S <Tomomi.Suzuki1@jp.ricoh.com>
2019-12-19 01:00:32 +0900

    13.6

---
+++ jpl/src/ch13/ex06/delimitedStringNumber.java
@@ -0,0 +1,30 @@
+package ch13.ex06;
+
+public class delimitedStringNumber {
+    public String addComma(String string, int digit) {
+        if (digit <= 0) {
+            throw new IllegalArgumentException("Digit should be positive!");
+        }
+
+        if (!string.matches("[0-9]+")) {
+            throw new IllegalArgumentException("Only numbers can be accepted!");
+        }
+
+        int length = string.length();
+        StringBuilder sb = new StringBuilder(string);
+        int offset = length % digit;
+
+        while (offset < length) {
+            if (offset == 0) {
+                offset += digit;
+            }
+            if (offset == length) {
+                break;
+            }
+            sb.insert(offset, ",");
+            offset = offset + digit + 1;
+        }
+
+        return sb.toString();
+    }
+}
---
+++ jpl/test/ch13/ex06/delimitedStringNumberTest.java
@@ -0,0 +1,52 @@
+package ch13.ex06;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class delimitedStringNumberTest {
+    private final int digit = 5;
+
+    @Test
+    public void digit桁区切りでカンマを入れる() {
+        String string = "1234567890";
+        delimitedStringNumber dsn = new delimitedStringNumber();
+
+        assertThat(dsn.addComma(string, digit), is("12345,67890"));
+    }
+
+    @Test
+    public void digit桁以下のときは何もしない() {
+        String string = "12345";
+        delimitedStringNumber dsn = new delimitedStringNumber();
+
+        assertThat(dsn.addComma(string, digit), is("12345"));
+    }
+
+    @Test
+    public void 数字以外が含まれたら例外を投げる() {
+        String string = "123abc456";
+        delimitedStringNumber dsn = new delimitedStringNumber();
+
+        try {
+            dsn.addComma(string, digit);
+        } catch (Exception actual) {
+            assertThat(actual.getMessage(), equalTo("Only numbers can be accepted!"));
+        }
+    }
+
+    @Test
+    public void digitが負の数なら例外を投げる() {
+        String string = "1234";
+        delimitedStringNumber dsn = new delimitedStringNumber();
+
+        try {
+            dsn.addComma(string, -3);
+        } catch (Exception actual) {
+            assertThat(actual.getMessage(), equalTo("Digit should be positive!"));
+        }
+    }
+
+}
