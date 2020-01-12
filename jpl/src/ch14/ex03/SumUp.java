3f820866b9cd3689fa49e48023f1a1cd05dc3b98
Tomom1-S <Tomomi.Suzuki1@jp.ricoh.com>
2020-01-12 11:54:39 +0900

    14.3

---
+++ jpl/src/ch14/ex03/SumUp.java
@@ -0,0 +1,39 @@
+package ch14.ex03;
+
+public class SumUp implements Runnable {
+    private int sum;
+    private int addition;
+    private int delay;
+
+    public SumUp (int addition, int delay) {
+        sum = 0;
+        this.addition = addition;
+        this.delay = delay;
+    }
+
+    public synchronized void add() {
+        int oldSum = sum;
+        sum += addition;
+        System.out.println(Thread.currentThread().getName() + ": " + oldSum + " + " + addition + " = " + sum);
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (;;) {
+                add();
+                Thread.sleep(delay);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Runnable sumUp = new SumUp(5, 1000);
+        new Thread(sumUp).start();
+        new Thread(sumUp).start();
+        new Thread(sumUp).start();
+    }
+}
