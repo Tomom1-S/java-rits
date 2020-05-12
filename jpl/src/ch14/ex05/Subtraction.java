17d170eaa7d342aa91d2ab7a42d0bec8cf3c5bfc
Tomom1-S <Tomomi.Suzuki1@jp.ricoh.com>
2020-05-13 00:01:26 +0900

    14.05

--- jpl/src/ch14/ex05/Subtraction.java
+++ jpl/src/ch14/ex05/Subtraction.java
@@ -1,4 +1,39 @@
 package ch14.ex05;
 
-public class Subtraction {
+public class Subtraction implements Runnable {
+    private int diff;
+    private int subtrahend;
+    protected final Object lock = new Object();
+
+    public Subtraction(int addition) {
+        diff = 0;
+        this.subtrahend = addition;
+    }
+
+    public void subtract() {
+        synchronized (lock) {
+            if (diff <= -100 || diff >= 100) {
+                return;
+            }
+
+            final int oldDiff = diff;
+            diff -= subtrahend;
+            System.out.println(Thread.currentThread().getName() + ": " + oldDiff + " + " + subtrahend + " = " + diff);
+        }
+    }
+
+    @Override
+    public void run() {
+        while (diff > -100 && diff < 100) {
+            subtract();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Runnable subtraction = new Subtraction(5);
+        new Thread(subtraction).start();
+        new Thread(subtraction).start();
+        new Thread(subtraction).start();
+    }
 }
