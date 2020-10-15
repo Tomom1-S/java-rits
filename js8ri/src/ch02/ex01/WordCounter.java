a21ca49b0ec7a75c40a200f9016a622c1ab1aa26
Tomom1-S <Tomomi.Suzuki1@jp.ricoh.com>
2020-10-15 22:21:15 +0900

    2.1

---
+++ js8ri/src/ch02/ex01/WordCounter.java
@@ -0,0 +1,63 @@
+package ch02.ex01;
+
+import ch01.ex11.S;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class WordCounter {
+    public List<String> initList(final String path) throws IOException {
+        String contents = new String(
+                Files.readAllBytes(Paths.get(path)),
+                StandardCharsets.UTF_8
+        );
+        List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
+        return words;
+    }
+
+    public int forCount(final List<String> words) {
+        int count = 0;
+        for (String w : words) {
+            if (w.length() > 12) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public long streamCount(final List<String> words) {
+        long count = words
+                .stream()
+                .filter(w -> w.length() > 12)
+                .count();
+        return count;
+    }
+
+    public long parallelCount(final List<String> words) {
+        long count = words
+                .parallelStream()
+                .filter(w -> w.length() > 12)
+                .count();
+        return count;
+    }
+
+    public int threadCount(final List<String> words) {
+        AtomicInteger count = new AtomicInteger(0);
+        for (String word : words) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    if (word.length() > 12) {
+                        count.getAndIncrement();
+                    }
+                }
+            }).start();
+        }
+        return count.get();
+    }
+}
