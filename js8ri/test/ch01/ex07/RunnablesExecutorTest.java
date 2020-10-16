4d1e845cad0c6b23857d558c25ee30736f8244db
Tomom1-S <Tomomi.Suzuki1@jp.ricoh.com>
2020-10-16 09:30:45 +0900

    1.7 Refactoring

--- js8ri/test/ch01/ex07/RunnablesExecutorTest.java
+++ js8ri/test/ch01/ex07/RunnablesExecutorTest.java
@@ -5,7 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.PrintStream;
 
 import static org.hamcrest.CoreMatchers.is;
