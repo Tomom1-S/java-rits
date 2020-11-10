d94d940a4de34c32b99638998ac5695168d82968
Tomom1-S <Tomomi.Suzuki1@jp.ricoh.com>
2020-11-10 21:24:45 +0900

    3.9

---
+++ js8ri/test/ch03/ex09/A.java
@@ -0,0 +1,8 @@
+package ch03.ex09;
+
+public class A extends B {
+    public String aPub = "this is a public field";
+    String aPackPri = "this is a package-private field";
+    protected String aPro = "this is a protected field";
+    private String aPri = "this is a private field";
+}
---
+++ js8ri/test/ch03/ex09/B.java
@@ -0,0 +1,8 @@
+package ch03.ex09;
+
+public class B extends C {
+    public String bPub = "this is a public field";
+    String bPackPri = "this is a package-private field";
+    protected String bPro = "this is a protected field";
+    private String bPri = "this is a private field";
+}
