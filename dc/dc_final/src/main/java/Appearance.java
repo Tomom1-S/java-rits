633432c8a938ab8c0dad836b8e310736f6aebffc
Tomom1-S <Tomomi.Suzuki1@jp.ricoh.com>
2021-05-04 09:57:33 +0900

    [WIP] Use prefs & fix default values of ComboBoxes

---
+++ dc/dc_final/src/main/java/Appearance.java
@@ -0,0 +1,15 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Appearance {
+    private double locX;
+    private double locY;
+    private String font;
+    private int fontSize;
+    private String fontColor;
+    private String bgColor;
+}
---
+++ dc/dc_final/src/main/java/AppearanceSetting.java
@@ -0,0 +1,15 @@
+public final class AppearanceSetting {
+    public static class PrefKey {
+        static final String LOC_X = "locX";
+        static final String LOC_Y = "locY";
+        static final String FONT = "font";
+        static final String FONT_SIZE = "fontSize";
+        static final String FONT_COLOR = "fontColor";
+        static final String BG_COLOR = "bgColor";
+    }
+
+    public static class Default {
+        static final double WINDOW_RATIO = 1.5;
+        static final int FONT_SIZE = 100;
+    }
+}
